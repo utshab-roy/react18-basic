f2c7d9da2ba71416c25b7872ac86feed0a21f7a6
Utshab Roy <utshab.roy@gmail.com>
2023-06-12 14:53:01 +0900

    click envet impl for alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+import Alert from './components/Alert';
 import Button from './components/Button';
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      <Button btnType="info" onClick={() => console.log('clicked')}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>Alert</Alert>
+      )}
+
+      <Button btnType="info" onClick={() => setAlertVisible(true)}>
         Click me !
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,12 +2,23 @@ import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode; // the props names need to be children and ReactNode need to be imported
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary" role="alert">
-      {children}
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>Holy guacamole!</strong> {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
     </div>
   );
 };
